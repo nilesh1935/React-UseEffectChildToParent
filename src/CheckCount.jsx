143a51cd05e2be47eaf0122d340473632fdafd9d
nilesh1935 <nilesh.kulk1234@gmail.com>
2022-05-24 13:20:59 +0530

    1 file modified

--- src/CheckCount.jsx
+++ src/CheckCount.jsx
@@ -12,18 +12,17 @@ const updateCount =(e)=>{
     setCount(0)
     setincCount(incCount+1)
   }
-  if(e.target.value.length % 3 === 0 ){
+  if(e.target.value.length % 3 === 0  && e.keyCode === 8){
     if(incCount > 0){
       setincCount(incCount-1)
     }
   }
-  
  }
   return(
     <>
     <h1>count: {incCount}</h1>
     <h1> Entered Text: {inputValue}</h1>
-    <input type="text"  onChange={updateCount}/>
+    <input type="text"  onKeyUp={updateCount}/>
     </>
   )
 }
