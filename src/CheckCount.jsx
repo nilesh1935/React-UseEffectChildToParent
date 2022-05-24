da915f44f843547d5d8329e6f230980c0d948525
nilesh1935 <nilesh.kulk1234@gmail.com>
2022-05-24 13:21:56 +0530

    1 file modified

--- src/CheckCount.jsx
+++ src/CheckCount.jsx
@@ -1,15 +1,13 @@
 import React,{useState} from "react";
 
 const CheckCount = () =>{
- const [count, setCount] = useState(0)
+ 
  const [incCount, setincCount] = useState(0)
  const [inputValue, setInputValue] = useState()
  
 const updateCount =(e)=>{
-  setCount(count+1)
   setInputValue(e.target.value )
   if(e.target.value.length % 3 === 0 ){
-    setCount(0)
     setincCount(incCount+1)
   }
   if(e.target.value.length % 3 === 0  && e.keyCode === 8){
